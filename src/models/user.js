71404080a96ffb996513b3435cccfb48d8ef3db6
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-05-16 14:40:50 +0530

    Deleting task for user

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("./task");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -51,21 +52,21 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
-userSchema.virtual('tasks', {
-  ref: 'Task',
-  localField: '_id',
-  foreignField: 'owner'
-})
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
+});
 
-userSchema.methods.toJSON = function(){
-  const user = this
-  const userObject = user.toObject()
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
 
-  delete userObject.password
-  delete userObject.tokens
+  delete userObject.password;
+  delete userObject.tokens;
 
-  return userObject
-}
+  return userObject;
+};
 
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
@@ -100,6 +101,12 @@ userSchema.pre("save", async function (next) {
     user.password = await bcrypt.hash(user.password, 8);
   }
 
+  next();
+});
+
+userSchema.pre("remove", async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
   next();
 });
 
